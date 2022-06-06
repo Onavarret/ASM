92b8dc87f8be8117bc2c7d7847330a7ce53a7974
Onavarret <106288049+Onavarret@users.noreply.github.com>
2022-06-06 13:39:06 -0500

    Add files via upload

---
+++ EUSART INTERRUPT.X/build/default/production/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/main.o:  \
+	/home/omars/MPLABXProjects/PIC16F887/EUSART\ INTERRUPT.X/main.asm  \
+	/home/omars/MPLABXProjects/PIC16F887/EUSART\ INTERRUPT.X/LCD_lib.inc  \
+
